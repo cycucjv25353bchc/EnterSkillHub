cac2e2388d1652004ef77b9301812bcdb89a8295
Aiswarya <aiswaryadurgasi@gmail.com>
2026-07-07 22:52:58 +0530

    Fixed API URLs

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -13,7 +13,7 @@ function Login() {
 
     try {
       const res = await axios.post(
-        'http://https://enterskillhub-production.up.railway.app/api/auth/login',
+        'https://enterskillhub-production.up.railway.app/api/auth/login',
         {
           email,
           password,
