dd17b503957151c5d9cacf62ebc52245bb0cf8e9
Aiswarya <aiswaryadurgasi@gmail.com>
2026-07-08 15:12:53 +0530

    Fix register API

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,90 +1,23 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+const registerUser = async () => {
+  try {
+    const res = await axios.post(
+      'https://enterskillhub-production.up.railway.app/api/auth/register',
+      {
+        name: user.name,
+        email: user.email,
+        password: user.password,
+      },
+    );
 
-function Register() {
-  const [user, setUser] = useState({
-    name: '',
-    email: '',
-    password: '',
-  });
+    alert(res.data.message);
+    window.location.href = '/login';
+  } catch (err) {
+    console.log(err);
 
-  const handleChange = (e) => {
-    setUser({
-      ...user,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const registerUser = async () => {
-    try {
-      const res = await axios.post(
-        'http://https://enterskillhub-production.up.railway.app/api/auth/register',
-        user,
-      );
-
-      alert(res.data.message);
-
-      window.location.href = '/login';
-    } catch (err) {
-      console.log(err.response);
-
-      if (err.response) {
-        alert(
-          err.response.data.message ||
-            err.response.data.error ||
-            'Registration Failed',
-        );
-      } else {
-        alert('Network Error');
-      }
+    if (err.response) {
+      alert(err.response.data.message);
+    } else {
+      alert('Network Error');
     }
-  };
-
-  return (
-    <div style={{ margin: '50px' }}>
-      <h1>EnterSkillHub</h1>
-      <h2>User Registration</h2>
-
-      <input
-        type="text"
-        name="name"
-        placeholder="Enter Name"
-        value={user.name}
-        onChange={handleChange}
-      />
-      <br />
-      <br />
-
-      <input
-        type="email"
-        name="email"
-        placeholder="Enter Email"
-        value={user.email}
-        onChange={handleChange}
-      />
-      <br />
-      <br />
-
-      <input
-        type="password"
-        name="password"
-        placeholder="Enter Password"
-        value={user.password}
-        onChange={handleChange}
-      />
-      <br />
-      <br />
-
-      <button onClick={registerUser}>Register</button>
-
-      <br />
-      <br />
-
-      <button onClick={() => (window.location.href = '/login')}>
-        Already have an account? Login
-      </button>
-    </div>
-  );
-}
-
-export default Register;
+  }
+};
